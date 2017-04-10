e705b9308b8fda12019e3f20205f09c18fd5c030
Anikait Singh <singh.anikait@gmail.com>
2017-04-09 22:17:35 -0400

    Initial Commit

---
+++ src/README.md
@@ -0,0 +1,9 @@
+# TrailBlazer
+This program finds a path through a maze given by a file.
+
+An example maze is the following:
+OPPPPPX
+
+The O is the starting point from which the computer starts from.
+Following a path designated by the letter P, the computer has to go from the O to the X.
+The computer may not go through the | symbols as they represent obstacles. Thus the computer has to go around it.
---
+++ src/numPaths/Coordinator.java
@@ -0,0 +1,69 @@
+package numPaths;
+
+import java.io.*;
+import java.util.ArrayList;
+
+/** Programming Challenge: Maze Route Finder
+ * @author anikaitsingh
+ */
+
+public class Coordinator {
+	
+	public static void main(String[] args){		
+		// instantiation and initialization
+		String inputStr;
+		ArrayList<String> arrL = new ArrayList<String>();
+		char[][] arr = null;
+		String file = "maze.txt";
+
+		// input maze.txt as lines of strings
+		try{
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			while((inputStr = reader.readLine())!=null){
+				arrL.add(inputStr);
+			}
+			reader.close();
+		}catch(FileNotFoundException ex){
+			ex.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		//arraylist of strings to 2D array of chars
+		if(arrL != null){
+			arr = new char[arrL.size()][arrL.get(0).length()];
+			for(int i = 0; i< arrL.size(); i++){
+				inputStr = arrL.get(i); //reuse of string
+				for(int j = 0; j< arrL.get(0).length(); j++){
+					arr[i][j] = inputStr.charAt(j);
+				}
+			}
+		}
+		
+		print2DCharArray(arr);
+		//maze finding algorithm
+		printCoordinateArray(MazeFinder.findPath(arr));
+	}
+	
+	
+	//prints a 2 dimensional character array
+	public static void print2DCharArray(char[][] arr){
+		for(int i = 0; i < arr.length; i++){
+			for(int j = 0; j < arr[0].length; j++){
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+		System.out.println("\n");
+	}
+	
+	
+	//prints a single dimensional Coordinate array
+	public static void printCoordinateArray(ArrayList<Coordinate> arr){
+		Coordinate coord;
+		for(int i = 0; i < arr.size(); i++){
+			coord = arr.get(i);
+			System.out.println("(" + coord.getX() + "," + coord.getY() + ")");
+		}
+	}
+}
